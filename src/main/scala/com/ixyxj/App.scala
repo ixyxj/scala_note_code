58ad535c16611c1c84b5c694a3a1ddddb7edac45
ixyxj <xyxjun@gmail.com>
2019-02-16 23:27:27 +0800

    init

---
+++ src/main/scala/com/ixyxj/App.scala
@@ -0,0 +1,13 @@
+package com.ixyxj
+
+/**
+  * Created by silen on 2019/2/16 23:10
+  * Copyright (c) 2019 in FORETREE
+  * 入口main函数
+  */
+object App {
+  def main(args: Array[String]): Unit = {
+    println("hello world!!!")
+  }
+
+}
